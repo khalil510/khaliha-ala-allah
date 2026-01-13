ab4aa6ef8db94ebce133a14ae8d6dd3ae4c0b21a
khalil510 <khalilabidi63@gmail.com>
2026-01-13 14:07:59 +0100

    adding footer to the modules

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Required f
 
 import { App } from '../app/app';
 import { ReservationFormComponent } from './reservation-form.component'; // Oussema's component
+import { FooterComponent } from './footer.component';
 import { ReservationListComponent } from './reservation-list.component'; // Amira's component
 import { ReservationService } from './../app/services/Reservation.Service'; // Hamza's service
 import { CommonModule } from '@angular/common';
@@ -12,7 +13,8 @@ import { CommonModule } from '@angular/common';
   declarations: [
     App,
     ReservationFormComponent, // Declare here
-    ReservationListComponent  // Declare here
+    ReservationListComponent,  // Declare here
+    FooterComponent
   ],
   imports: [
     BrowserModule,
